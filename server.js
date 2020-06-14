d9288ecbfc2535cf9d377223a1aeb3ca16a23c92
Jsquad <lakis006@umn.edu>
2020-06-14 12:12:14 -0500

    added more code to the server.js to start the process of adding a new note using the POST method

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(_dirname, "public/notes.html"));
 });
 
+const noteData = [];
 
 // when the data is collected it will end up as db.json here
 
@@ -28,6 +29,31 @@ app.get("/notes", function (req, res) {
 
 // use POST method to add a note
 
-app.post("/api/notes",)
+app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    
+    //adds the new note
+    fs.readFile("db/db.json", "utf8", function (err, input) {
+        if (err) throw err;
+
+
+    })
+
+    
+
+})
+
+// use DELETE method to be able to delete note 
+
+
 
 //render everything on the main page
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(_dirname, "/public/index.html"));
+});
+
+//This is where the server will commence 
+app.listen(PORT, function() {
+    console.log("App listening on PORT: ", PORT);
+});
