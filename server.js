2730ffd13c6ec46247810423903dfa4e5a3ca7f6
Jsquad <lakis006@umn.edu>
2020-06-20 23:44:57 -0500

    changed up the app.post and app.delete because app wasn't functional

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const fs = require("fs");
 const express = require("express");
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+const util = require('util');
+const readAsync = util.promisify(fs.readFile);
+const writeAsync = util.promisify(fs.writeFile);
 
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 9000;
 
 
 app.use(express.urlencoded({ extended: true}));
@@ -16,68 +20,60 @@ let storedInfo = [];
 //the routes that will render notes 
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(_dirname, "/public/notes.html"));
+    res.sendFile(path.join(_dirname, "public/notes.html"));
 });
 
 // when the data is collected it will end up as db.json here
 
-app.get("/api/notes", function (req, res) {
-    fs.readFile("db/db.json", "utf8", (err, data) => {
-        let theData = JSON.parse(data);
-        return res.json(theData);
-    });
+app.get("/api/notes", async (req, res) {
+    try {
+        return res.json(JSON.parse(await readAsync(path.join(_dirname, "/db/db.json"), "utf8")));
+    } catch (err) {
+        console.log(err);
+    }
 });
 
 
 // use POST method to add a note
 
-app.post("/api/notes", function (req, res) {
-    let newNote = req.body;
-   
-    //adds the new note
-    fs.readFile("db/db.json", "utf8", function (err, input) {
-        if (err) throw err;
-        let storedInfo = JSON.parse(input);
-        storedInfo.push(newNote);
-      
-        for (let i in storedInfo) {
-            storedInfo[i].id = parseInt([i]) + 1;
-        }
+app.post("/api/notes", async (req, res) {
+   try {
+       let newNote = JSON.parse(JSON.stringify(req.body));
+       newNote.id = uuidv4();
+       console.log(newNote);
 
-        fs.writeFile("db/db.json", JSON.stringify(storedInfo), function(err) {
-            if (err) throw err;
-        });
-        res.json(true);
-    });
+       let newArray = JSON.parse(await readAsync(path.join(_dirname, "/db/db.json"), "utf-8"));
+       newArray.push(newNote);
+       await writeAsync(path.join(_dirname, "/db/db.json"), JSON.stringify(newArray));
+
+       return res.json(newNote);
+   } catch (err) {
+       console.log(err);
+   }
 });
 
 // use DELETE method to be able to delete note 
-app.delete("/api/notes/:id", function (req,res) {
-    let deleteDirectory = req.params.id;
-   
-
-
-    fs.readFile("db/db.json", 'utf8', function (err, input) {
-        if (err) throw err;
-        storedInfo = JSON.parse(input);
-        storedInfo.splice(deleteDirectory - 1, 1);
-
-        for (let i in storedInfo) {
-            storedInfo[i].id = parseInt([i]) + 1;
+app.delete("api/notes/:id", function (req,res) {
+   console.log("It deletes!!");
+   let newDelete = req.params.id;
+   let newData = fs.readFileSync(path.join(_dirname, "/db/db.json"), 'utf-8');
+    let newArray = JSON.parse(newData);
+    for (note of newArray) {
+        if (note.id == newDelete) {
+            newArray.pop(note);
+            fs.writeFileSync(path.join(_dirname, "/db/db.json"), JSON.stringify(newArray));
         }
+    }
 
-    
-        fs.writeFile("db/db.json", JSON.stringify(storedInfo), function(err) {
-            if (err) throw err;
-        });
-    });
-    res.json(storedInfo);
+    res.json({message: "File Deleted"})
 });
 
 
-//render everything on the main page
+//render everything on the main page and makes sure get request is working 
 
-
+app.get("*", function (req, res) {
+    res.sendfile(path.join(_dirname, "public/index.html"));
+});
 
 //This is where the server will commence 
 app.listen(PORT, function() {
